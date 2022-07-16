6c58d8b71a08ddbb5626ee091b8476d151943257
ManishSharma1609 <104347990+ManishSharma1609@users.noreply.github.com>
2022-07-16 23:31:24 +0530

    Add files via upload

---
+++ Output 7.py
@@ -0,0 +1,30 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:/Manish/New folder (2)/Output 7.py ================
+Enter size of list: 6
+Enter marks: 10
+Enter marks: 11
+Enter marks: 50
+Enter marks: 22
+Enter marks: 65
+Enter marks: 99
+[10, 11, 22, 50, 65, 99]
+Enter size of array: 5
+Enter element 5
+Enter element 8
+Enter element 8
+Enter element 8
+Enter element 4
+[4, 5, 8, 8, 8]
+Enter element to be searched and counted: 8
+Element is present at index 2
+Number of occurrences of element is 3
+Enter number of inputs:6
+Enter number:5
+Enter number:5
+Enter number:9
+Enter number:0
+Enter number:0
+Enter number:1
+[0, 1, 5, 9]
