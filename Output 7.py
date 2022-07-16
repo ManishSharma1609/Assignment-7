3a48989bab42ed114981d8224cfb4da960455ad3
ManishSharma1609 <104347990+ManishSharma1609@users.noreply.github.com>
2022-07-16 23:32:33 +0530

    Update Output 7.py

--- Output 7.py
+++ Output 7.py
@@ -2,6 +2,7 @@ Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 
 ================= RESTART: D:/Manish/New folder (2)/Output 7.py ================
+#Q4
 Enter size of list: 6
 Enter marks: 10
 Enter marks: 11
@@ -10,6 +11,9 @@ Enter marks: 22
 Enter marks: 65
 Enter marks: 99
 [10, 11, 22, 50, 65, 99]
+
+
+#Q5
 Enter size of array: 5
 Enter element 5
 Enter element 8
@@ -20,6 +24,9 @@ Enter element 4
 Enter element to be searched and counted: 8
 Element is present at index 2
 Number of occurrences of element is 3
+
+
+#Q6
 Enter number of inputs:6
 Enter number:5
 Enter number:5
